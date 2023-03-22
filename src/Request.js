e7a3df9489e41f3aeab6d2eb6568f3f8fb78a4f4
Sergio <sergiof_2001@hotmail.com>
2023-03-21 19:48:47 -0700

    done with ui

--- src/Request.js
+++ src/Request.js
@@ -6,8 +6,8 @@ const request = {
     requestPopular: `
     https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-    requestTrending:`ttps://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
-    requestTv: `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=1`,
+    requestTrending:`https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+    requestTv: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Main from '../components/Main'
-import Row from '../components/Row'
-import request from '../Request'
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import request from "../Request";
 const Home = () => {
   return (
     <div>
-      <Main/>
-      <Row title='Up Coming' fetchURL={request.requestUpcoming} />
-      <Row title='Popular' fetchURL={request.requestPopular} />
-      <Row title='Top Rated' fetchURL={request.requestTopRated} />
-      <Row title='Trending' fetchURL={request.requestTrending} />
-      <Row title='Television Shows' fetchURL={request.requestTv} />
+      <Main />
+      <Row rowId="1" title="Up Coming" fetchURL={request.requestUpcoming} />
+      <Row rowId="2" title="Popular" fetchURL={request.requestPopular} />
+      <Row rowId="3" title="Top Rated" fetchURL={request.requestTopRated} />
+      <Row rowId="4" title="Trending" fetchURL={request.requestTrending} />
+      <Row rowId="5" title="Television Shows" fetchURL={request.requestTv} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
