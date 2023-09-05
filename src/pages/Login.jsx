b777f3a0ae2d926d7c6b62daa7d8d785d42e37a6
Sergio Aguilar <sergiof_2001@hotmail.com>
2023-09-05 12:31:45 -0700

    new updates on the login screen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      
+    <>
+    <div className="w-full h-screen">
+      <img
+        className="hidden sm:block absolute w-full h-full object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/58126e35-739d-409f-9920-e213b5e2d640/US-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        alt="/"
+      />
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"> </div>
+      <div className="fixed w-full px-4 py-24 z-50">
+        <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[320px] mx-auto py-16">
+            <h1 className="text-3xl font-bold">Sign In</h1>
+            <form className="w-full flex flex-col py-4">
+              <input
+                className="p-3 my-2 bg-gray-700 rounded"
+                type="email"
+                placeholder="Email"
+              />
+              <input
+                className="p-3 my-2 bg-gray-700 rounded"
+                type="password"
+                placeholder="Password"
+              />
+              <button className="bg-red-600 py-3 my-6 rounded font-bold">
+               Login
+              </button>
+              <div className="flex justify-between items-center">
+                <p>
+                  <input className="mr-2" type="checkbox" />
+                  Remember Me
+                </p>
+                <p>Need Help?</p>
+              </div>
+              <p className="py-8">
+                <span className="text-gray-600">Want to become a Netflix Member?       </span>
+                <Link to='/signup'>
+                Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  </>
+  );
+};
 
-export default Login
+export default Login;
