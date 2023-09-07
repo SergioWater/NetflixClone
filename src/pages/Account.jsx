dc38eaa217c3efbc49174f1ed5c9a958f03b0166
Sergio Aguilar <sergiof_2001@hotmail.com>
2023-09-07 14:16:17 -0700

    account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,10 +1,20 @@
-
-const Account = ( ) => {
-return(
+const Account = () => {
+  return (
     <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/58126e35-739d-409f-9920-e213b5e2d640/US-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]  "></div>
+        <div className="absoulte top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My shows</h1>
+        </div>
+      </div>
+      
     </>
-)
+  );
+};
 
-}
-
-export default Account
+export default Account;
